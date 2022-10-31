54143c3020a08fb56047bd59a0d7e0ea30d47187
IvanVictor85 <ivanvoliveira@gmail.com>
2022-10-31 11:40:26 -0300

    Inclusao de comentarios e zeros a esquerda nos IDs

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,7 +3,7 @@ const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
+    pokemon.number = ("000" + pokeDetail.id).slice(-3) //mudança para ID e tratamento para inclusão dos zeros a esquerda
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
@@ -19,17 +19,18 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+    .then((response) => response.json()) // Fazer uma nova busca de cada detalhe individual dos pokemons via url e transformar num Json
+    .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+    return fetch(url) //Fazer requesição da lista de pokemons
+        .then((response) => response.json()) // Converte o http response(resultado da busca) para Json
+        .then((jsonBody) => jsonBody.results) // Filtrar apenas os resultados que importam (lista de pokemons)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) // Mapeia a lista de pokemons e consverte em uma lista de detalhes dos pokemons
+        .then((detailRequests) => Promise.all(detailRequests)) // Esperar que as requisições terminem e seja convertida em uma lista de Json de detalhes
+        .then((pokemonsDetails) => pokemonsDetails) //Lista de detalhes dos pokemons
+        //.catch((error) => console.error(error)) 
 }
